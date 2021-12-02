eff74d56a6caa35b8e5be106568fe9c62b4a5231
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-02 08:41:07 -0300

    Add da pagina carteira

--- apps/crypto/urls.py
+++ apps/crypto/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Index
+from .views import Index, Carteira
 
 urlpatterns = [
 
     #ListView
     path('', Index.as_view()),
+    path('carteira/', Carteira.as_view()),
 
 
 ]
--- apps/crypto/views.py
+++ apps/crypto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Crypto
+from .models import Crypto, Active
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -12,5 +12,14 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cryptos'] = Crypto.objects.all()
+        return context
+
+class Carteira(TemplateView):
+    model = Active
+    template_name = 'crypto/carteira.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(Carteira, self).get_context_data()
+        context['actives'] = Active.objects.all()
         return context
 
