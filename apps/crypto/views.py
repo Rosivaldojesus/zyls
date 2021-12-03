1de47c614673b488a9b8bf0fe30a76f41304c12e
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-02 21:53:59 -0300

    refatorando pagina carteira

--- apps/crypto/views.py
+++ apps/crypto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import F, Q
 from .models import Crypto, Active
+import requests
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -13,6 +14,11 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cryptos'] = Crypto.objects.all()
+        context['crypt'] = requests.get('https://www.mercadobitcoin.net/api/SOL/ticker/').json()
+        crypt = requests.get('https://www.mercadobitcoin.net/api/SOL/ticker/').json()
+        context['solana'] = crypt['ticker']["last"]
+
+
         return context
 
 class Carteira(TemplateView):
@@ -21,11 +27,18 @@ class Carteira(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super(Carteira, self).get_context_data()
+
+        context['crypt'] = requests.get('https://www.mercadobitcoin.net/api/SOL/ticker/').json()
+        crypt = requests.get('https://www.mercadobitcoin.net/api/SOL/ticker/').json()
+        solana = crypt['ticker']["last"]
+
+
+
         context['actives'] = Active.objects.annotate(
-            lucro=(F('name_crypto__value_current_cripto') * F('quantity_crypto')) - F('purchase_value')
-
-
-
+            lucro=( solana * F('quantity_crypto')) - F('purchase_value')
         )
+
+
+
         return context
 
