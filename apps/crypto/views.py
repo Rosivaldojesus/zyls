bca54c83b5f1784e1769ea1b529ed2635c3fd159
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-06 23:14:25 -0300

    Add porcentagem para cryptos

--- apps/crypto/views.py
+++ apps/crypto/views.py
@@ -65,8 +65,8 @@ class Saldos(TemplateView):
 
         #Cardano
         context['valor_atual_cardano'] = Cardano.valor_atual_cardano(self)
-        context['percentual'] = Active.objects.filter(name_crypto__crypto_symbol='ADA').annotate(
-            percental=(valor_cardano * 100))
+        context['percentual'] = Active.objects.filter(name_crypto__crypto_symbol='ADA')\
+            .annotate(lucro=(valor_cardano * F('quantity_crypto')) - F('purchase_value'))
 
         context['cardano'] = Active.objects.filter(name_crypto__crypto_symbol='ADA')\
         .annotate(lucro=(valor_cardano * F('quantity_crypto')) - F('purchase_value'))
