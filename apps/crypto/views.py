03ce2fa12e863dfbe7a5fba7042ab5f4f2af10c3
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-07 08:00:29 -0300

    Add porcentagem para cryptos

--- apps/crypto/views.py
+++ apps/crypto/views.py
@@ -61,23 +61,23 @@ class Saldos(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Saldos, self).get_context_data()
-        valor_cardano = Cardano.valor_atual_cardano(self)
+        #valor_cardano = Cardano.valor_atual_cardano(self)
 
         #Cardano
         context['valor_atual_cardano'] = Cardano.valor_atual_cardano(self)
-        context['percentual'] = Active.objects.filter(name_crypto__crypto_symbol='ADA').annotate(
-            lucro=((100 / F('unitary_value')) * valor_cardano) - 100)
+        context['percentual_ada'] = Active.objects.filter(name_crypto__crypto_symbol='ADA').annotate(
+            porcentagem=((100 / F('unitary_value')) * Cardano.valor_atual_cardano(self)) - 100)
         context['cardano'] = Active.objects.filter(name_crypto__crypto_symbol='ADA').annotate(
-            lucro=(valor_cardano * F('quantity_crypto')) - F('purchase_value'))
+            lucro=(Cardano.valor_atual_cardano(self) * F('quantity_crypto')) - F('purchase_value'))
 
 
         # AMP
-        valor_amp = Amp.valor_atual_amp(self)
-        context['valor_atual_amp'] = valor_amp
-        # context['percentual'] = Active.objects.filter(name_crypto__crypto_symbol='AMP').annotate(
-        #     lucro=((100 / F('unitary_value')) * valor_amp) - 100)
+        #valor_amp = Amp.valor_atual_amp(self)
+        context['valor_atual_amp'] = Amp.valor_atual_amp(self)
+        # context['percentual_amp'] = Active.objects.filter(name_crypto__crypto_symbol='AMP').annotate(
+        #      porcentagem=((100 / F('unitary_value')) * Amp.valor_atual_amp(self)) - 100)
         context['amp'] = Active.objects.filter(name_crypto__crypto_symbol='AMP').annotate(
-            lucro=(valor_amp * F('quantity_crypto')) - F('purchase_value'))
+            lucro=(Amp.valor_atual_amp(self) * F('quantity_crypto')) - F('purchase_value'))
 
         # BNT
         bnt = requests.get('https://www.mercadobitcoin.net/api/BNT/ticker/').json()['ticker']["last"]
