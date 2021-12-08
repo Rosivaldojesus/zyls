57915a0534a91e79447d8ddf071d41ab57f7755c
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-07 23:53:16 -0300

    Add porcentagem SUSHI

--- apps/crypto/classes/valores_cryptos.py
+++ apps/crypto/classes/valores_cryptos.py
@@ -2,90 +2,60 @@ import requests
 
 
 def valor_atual_cardano():
-     url = 'https://www.mercadobitcoin.net/api/ADA/ticker/'
-     valor_cardano = requests.get(url).json()['ticker']["last"]
-     return valor_cardano
+    valor_cardano = requests.get('https://www.mercadobitcoin.net/api/ADA/ticker/').json()['ticker']["last"]
+    return valor_cardano
 
 
 def valor_atual_amp():
-     url = 'https://www.mercadobitcoin.net/api/AMP/ticker/'
-     valor_amp = requests.get(url).json()['ticker']["last"]
-     return valor_amp
-
+    valor_amp = requests.get('https://www.mercadobitcoin.net/api/AMP/ticker/').json()['ticker']["last"]
+    return valor_amp
 
 
 def valor_atual_bnt():
-     url = 'https://www.mercadobitcoin.net/api/BNT/ticker/'
-     valor_bnt = requests.get(url).json()['ticker']["last"]
-     return valor_bnt
-
-
-
-
+    valor_bnt = requests.get('https://www.mercadobitcoin.net/api/BNT/ticker/').json()['ticker']["last"]
+    return valor_bnt
 
 
 def valor_atual_btc():
-     url = 'https://www.mercadobitcoin.net/api/BTC/ticker/'
-     valor_btc = requests.get(url).json()['ticker']["last"]
-     return valor_btc
-
+    valor_btc = requests.get('https://www.mercadobitcoin.net/api/BTC/ticker/').json()['ticker']["last"]
+    return valor_btc
 
 
 def valor_atual_city():
-     url = 'https://www.mercadobitcoin.net/api/CITYFT/ticker/'
-     valor_city = requests.get(url).json()['ticker']["last"]
-     return valor_city
-
-
-
+    valor_city = requests.get('https://www.mercadobitcoin.net/api/CITYFT/ticker/').json()['ticker']["last"]
+    return valor_city
 
 
 def valor_atual_crv():
-     url = 'https://www.mercadobitcoin.net/api/CRV/ticker/'
-     valor_crv = requests.get(url).json()['ticker']["last"]
-     return valor_crv
-
+    valor_crv = requests.get('https://www.mercadobitcoin.net/api/CRV/ticker/').json()['ticker']["last"]
+    return valor_crv
 
 
 def valor_atual_doge():
-     url = 'https://www.mercadobitcoin.net/api/DOGE/ticker/'
-     valor_doge = requests.get(url).json()['ticker']["last"]
-     return valor_doge
-
-
-
+    valor_doge = requests.get('https://www.mercadobitcoin.net/api/DOGE/ticker/').json()['ticker']["last"]
+    return valor_doge
 
 
 def valor_atual_mana():
-     url = 'https://www.mercadobitcoin.net/api/MANA/ticker/'
-     valor_mana = requests.get(url).json()['ticker']["last"]
-     return valor_mana
-
-
+    valor_mana = requests.get('https://www.mercadobitcoin.net/api/MANA/ticker/').json()['ticker']["last"]
+    return valor_mana
 
 
 def valor_atual_sol():
-    url = 'https://www.mercadobitcoin.net/api/SOL/ticker/'
-    valor_sol = requests.get(url).json()['ticker']["last"]
+    valor_sol = requests.get('https://www.mercadobitcoin.net/api/SOL/ticker/').json()['ticker']["last"]
     return valor_sol
 
 
-
 def valor_atual_sushi():
-     url = 'https://www.mercadobitcoin.net/api/SUSHI/ticker/'
-     valor_sushi = requests.get(url).json()['ticker']["last"]
-     return valor_sushi
-
+    valor_sushi = requests.get('https://www.mercadobitcoin.net/api/SUSHI/ticker/').json()['ticker']["last"]
+    return valor_sushi
 
 
 def valor_atual_wbx():
-    url = 'https://www.mercadobitcoin.net/api/WBX/ticker/'
-    valor_wbx = requests.get(url).json()['ticker']["last"]
+    valor_wbx = requests.get('https://www.mercadobitcoin.net/api/WBX/ticker/').json()['ticker']["last"]
     return valor_wbx
 
 
-
 def valor_atual_zrx():
-    url = 'https://www.mercadobitcoin.net/api/ZRX/ticker/'
-    valor_zrx = requests.get(url).json()['ticker']["last"]
+    valor_zrx = requests.get('https://www.mercadobitcoin.net/api/ZRX/ticker/').json()['ticker']["last"]
     return valor_zrx
