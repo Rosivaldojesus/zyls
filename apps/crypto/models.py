c895223940a89c0c444f20e29c164e06f5dc88ad
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-12-27 20:14:20 -0300

    Add zrx

--- apps/crypto/models.py
+++ apps/crypto/models.py
@@ -28,8 +28,8 @@ class Crypto(models.Model):
 
 class Active(models.Model):
     name_crypto = models.ForeignKey(Crypto, on_delete=models.DO_NOTHING)
-    unitary_value = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    purchase_value = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    unitary_value = models.DecimalField(max_digits=8, decimal_places=4, default=0)
+    purchase_value = models.DecimalField(max_digits=8, decimal_places=4, default=0)
     quantity_crypto = models.DecimalField(max_digits=19, decimal_places=14, default=0)
     purchase_date = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
